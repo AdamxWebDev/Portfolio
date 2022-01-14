9f79bc218bf7a35b339ed81007f4f4582cabf9ba
Adam McCartney <Adam_WebDev@outlook.com>
2022-01-13 19:29:08 -0700

    Added Animations and Content

--- script.js
+++ script.js
@@ -11,24 +11,16 @@ const isAnimated = element => (
 
 const intersectionObserver = new IntersectionObserver((entries, observer) => {
   entries.forEach((entry) => {
-    
-    // when element's is in viewport,
-    // animate it!
     if (entry.intersectionRatio > 0) {
       animate(entry.target);
-      // remove observer after animation
       observer.unobserve(entry.target);
     }
   });
 });
 
-// get only these elements,
-// which are not animated yet
 const elements = [].filter.call(
   document.querySelectorAll(SELECTOR),
   element => !isAnimated(element, ANIMATE_CLASS_NAME)
 );
-//console.log(elements);
 
-// start observing your elements
 elements.forEach((element) => intersectionObserver.observe(element));
